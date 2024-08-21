ddfec20ba2f4791b39ec88e31dcb01b5221476f5
Tiago Vieira <98527646+TiagoCostaVieira@users.noreply.github.com>
2024-08-21 16:00:13 -0300

    Finalizado

--- contracts/Cortex.sol
+++ contracts/Cortex.sol
@@ -17,10 +17,26 @@ contract Cortex is ERC721, ERC721Enurable, ERC721URIStorage, ERC721Burnable{
         {
             return super.supportsInterface(interfaceId);
         }
+    //METADADOS
     function _baseURI() interal pure override returns(string memory){
         return "https://www.luiztools.com.br/nft/";
     }
     function tokenURI(uint256 tokenID) public view override(ERC721, ERC721URIStorage) returns(string memory){
         return string.concat(super.tokenURI(tokenID), ".json");
+    }
+
+    function mint() public{
+        uint256 tokenId = ++_tokenIdCounter;
+
+        _safeMint(msg.sender, tokenId);
+        _safeTokenURI(tokenId, String.toString(tokenId));
+    }
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
     }
 }
